cc1c4790f32528f55954fe3f2bdef06cbe1692d2
DenisSamoilyk <deniskakbsamoilyk@gmail.com>
2023-10-13 18:32:24 +0300

    Calculatorv1

---
+++ Calculator.py
@@ -0,0 +1,23 @@
+try:
+    number1 = int(input("Write the first number:"))
+    number2 = int(input("Write the second number:"))
+    print("\n1 addition \n2 subtraction \n3 division \n4 multiplication")
+    operator = int(input("Select operation:"))
+    if operator == 1:
+        result = number1 + number2
+    elif operator == 2:
+        result = number1 - number2
+    elif operator == 3:
+        result = number1 / number2
+    elif operator == 4:
+        result = number1 * number2
+    else:
+        print("Wrong operator")
+    print(result)
+# Possible Errors
+except ZeroDivisionError:
+    print("You can't divide zero")
+except ValueError:
+    print("Error. A letter was written or nothing was written")
+except Exception:
+    print("Something went wrong")
