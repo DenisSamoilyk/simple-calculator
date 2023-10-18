be831d169a142a49ab9574c4f8af511455724c66
DenisSamoilyk <deniskakbsamoilyk@gmail.com>
2023-10-18 21:46:45 +0300

    Calculatorv2

--- CalculatorV2.py
+++ CalculatorV2.py
@@ -21,71 +21,93 @@ while True:
             print("Okay, bye")
             break
         elif find_Calculator == "calculator" or write == "1":
+        # Calculator
+            while True:
+                try:
+                    print("▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣")
+                    number1 = float(input("Write the first number: "))
+                    number2 = float(input("Write the second number: "))
+                    print("▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣")
+                    print("1: addition \n2: subtraction \n3: division \n4: multiplication \n")
+                    write_operator = input("Select operation: ")
+                    word = write_operator.lower()
+                    result = "Error in solution"
+                    operator = " and "
+                # find Addition
+                    Addition_first_letter = write_operator.find("a")
+                    Addition_last_letter = write_operator.find("n")
+                    find_Addition = word[Addition_first_letter:(Addition_last_letter + 1)]
+                # find Subtraction
+                    Subtraction_first_letter = write_operator.find("s")
+                    Subtraction_last_letter = write_operator.find("n")
+                    find_Subtraction = word[Subtraction_first_letter:(Subtraction_last_letter + 1)]
+                # find Division
+                    Division_first_letter = write_operator.find("d")
+                    Division_last_letter = write_operator.find("n")
+                    find_Division = word[Division_first_letter:(Division_last_letter + 1)]
+                # find Multiplication
+                    Multiplication_first_letter = write_operator.find("m")
+                    Multiplication_last_letter = write_operator.find("n")
+                    find_Multiplication = word[Multiplication_first_letter:(Multiplication_last_letter + 1)]
 
-            # Calculator
-            print("▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣")
-            number1 = float(input("Write the first number: "))
-            number2 = float(input("Write the second number: "))
-            print("▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣")
-            print("1: addition \n2: subtraction \n3: division \n4: multiplication \n")
-            write_operator = input("Select operation: ")
-            word = write_operator.lower()
-            result = "Error in solution"
-            # find Addition
-            Addition_first_letter = write_operator.find("a")
-            Addition_last_letter = write_operator.find("n")
-            find_Addition = word[Addition_first_letter:(Addition_last_letter + 1)]
-            # find Subtraction
-            Subtraction_first_letter = write_operator.find("s")
-            Subtraction_last_letter = write_operator.find("n")
-            find_Subtraction = word[Subtraction_first_letter:(Subtraction_last_letter + 1)]
-            # find Division
-            Division_first_letter = write_operator.find("d")
-            Division_last_letter = write_operator.find("n")
-            find_Division = word[Division_first_letter:(Division_last_letter + 1)]
-            # find Multiplication
-            Multiplication_first_letter = write_operator.find("m")
-            Multiplication_last_letter = write_operator.find("n")
-            find_Multiplication = word[Multiplication_first_letter:(Multiplication_last_letter + 1)]
+                    if find_Addition == "addition" or write_operator == "1" or write_operator == "+":
+                        result = number1 + number2
+                        operator = "+"
+                    elif find_Subtraction == "subtraction" or write_operator == "2" or write_operator == "-":
+                        result = number1 - number2
+                        operator = "-"
+                    elif find_Division == "division" or write_operator == "3" or write_operator == "/":
+                        result = number1 / number2
+                        operator = "/"
+                    elif find_Multiplication == "multiplication" or write_operator == "4" or write_operator == "*":
+                        result = number1 * number2
+                        operator = "*"
+                    else:
+                        print("┏◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚┓")
+                        print(" You write something wrong.")
+                        print(" TIP: Try the whole word or choose one of the numbers")
+                        print("┗◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛┛")
+                # Result
+                    print("▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣")
+                    print(f"{number1}{operator}{number2} = {result}")
+                    print("▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣")
+                # Continue?
+                    ask_question = input("Do you want continue?\nYes/No\n")
+                    L_ask_question = ask_question.lower()
+                    if L_ask_question == "yes" or L_ask_question == "ye" or L_ask_question == "y":
+                        continue
+                    elif L_ask_question == "no" or L_ask_question == "n":
+                        print("▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣")
+                        break
+                    else:
+                        print("|◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢|")
+                        print(" You write something wrong, so we continue")
+                        print("|◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢|")
 
-            if find_Addition == "addition" or write_operator == "1" or write_operator == "+":
-                result = number1 + number2
-            elif find_Subtraction == "subtraction" or write_operator == "2" or write_operator == "-":
-                result = number1 - number2
-            elif find_Division == "division" or write_operator == "3" or write_operator == "/":
-                result = number1 / number2
-            elif find_Multiplication == "multiplication" or write_operator == "4" or write_operator == "*":
-                result = number1 * number2
-            else:
-                print("┏◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚┓")
-                print(" You write something wrong.")
-                print(" TIP: Try the whole word or choose one of the numbers")
-                print("┗◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛◛┛")
-                continue
-            print("▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣")
-            print(result)
-            print("▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣")
+            # Possible Errors
+                except ZeroDivisionError:
+                    print("|◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢|")
+                    print(" You can't divide zero")
+                    print("|◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢|")
+                except ValueError:
+                    print("|◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢|")
+                    print(" Error. You write something wrong or nothing was written")
+                    print("|◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢|")
+                except Exception as result:
+                    print("|◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢|")
+                    print(f"Something went wrong: {result}")
+                    print("|◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢|")
 
         elif find_Continue == "continue" or write == "2":
             print("▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣")
             print("There's nothing here yet")
             print("▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣")
-            continue
         else:
             print("|◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢|")
             print(" Error. You write something wrong or nothing was written")
             print("|◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢|")
-
 # Possible Errors
-    except ZeroDivisionError:
-        print("|◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢|")
-        print(" You can't divide zero")
-        print("|◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢|")
-    except ValueError:
-        print("|◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢|")
-        print(" Error. You write something wrong or nothing was written")
-        print("|◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢|")
-    except Exception as result:
+    except Exception as write:
         print("|◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢|")
-        print( f"Something went wrong: {result}")
+        print( f"Something went wrong: {write}")
         print("|◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢◤◢|")
